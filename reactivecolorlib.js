667cb953f23e96edf23999420aaabc33f4f25e21
James Tomasino <git@jamestomasino.com>
2012-08-13 13:24:42 -0400

    added proxy. Updated page dramatically

--- reactivecolorlib.js
+++ reactivecolorlib.js
@@ -1,97 +1,117 @@
 // Function source from: http://stackoverflow.com/questions/2541481/get-average-color-of-image-via-javascript
-function getAverageRGB(imgEl) {
-
+function getAverageRGB( img )
+{
 	var blockSize = 5,
-	defaultRGB = {r:0,g:0,b:0},
+	defaultRGB = [0,0,0],
 	canvas = document.createElement('canvas'),
 	context = canvas.getContext && canvas.getContext('2d'),
 	data, width, height,
 	i = -4,
 	length,
-	rgb = {r:0,g:0,b:0},
+	rgb = [0,0,0];
 	count = 0;
 
-	if (!context) {
+	if (!context)
+	{
 		return defaultRGB;
 	}
 
-	height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-	width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+	height = canvas.height = img.naturalHeight || img.offsetHeight || img.height;
+	width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
 
-	context.drawImage(imgEl, 0, 0);
+	context.drawImage(img, 0, 0);
 
-	try {
+	try
+	{
 		data = context.getImageData(0, 0, width, height);
-	} catch(e) {
-		/* security error, img on diff domain */
-		alert('Image is on a different domain. Cannot access color info with canvas');
+	}
+	catch(e)
+	{
+		//alert('Image is on a different domain. Cannot access color info with canvas');
 		return defaultRGB;
 	}
 
 	length = data.data.length;
 
-	while ( (i += blockSize * 4) < length ) {
+	while ( (i += blockSize * 4) < length )
+	{
 		++count;
-		rgb.r += data.data[i];
-		rgb.g += data.data[i+1];
-		rgb.b += data.data[i+2];
+		rgb[0] += data.data[i];
+		rgb[1] += data.data[i+1];
+		rgb[2] += data.data[i+2];
 	}
 
 	// ~~ used to floor values
-	rgb.r = ~~(rgb.r/count);
-	rgb.g = ~~(rgb.g/count);
-	rgb.b = ~~(rgb.b/count);
+	rgb[0] = ~~(rgb[0] / count);
+	rgb[1] = ~~(rgb[1] / count);
+	rgb[2] = ~~(rgb[2] / count);
 
-	var r = Math.round(rgb.r).toString(16);
-	var g = Math.round(rgb.g).toString(16);
-	var b = Math.round(rgb.b).toString(16);
-
-	if (r.length == 1) r = '0' + r;
-	if (g.length == 1) g = '0' + g;
-	if (b.length == 1) b = '0' + b;
-
-	return r + g + b;
+	return rgb;
 }
 
-function rgb2hsvl (rgbHex)  // RGB Values:Number 0-255, HSVL Results:Number 0-1
+function rgb2hsvly ( r, g, b )
 {
-	var R,G,B;
-
-	// strip #
-	if (rgbHex.slice(0,1) == '#') rgbHex = rgbHex.slice(1);
-
-	// Parse to 0-255 values
-	R = parseInt( rgbHex.slice(0,2), 16 );
-	G = parseInt( rgbHex.slice(2,4), 16 );
-	B = parseInt( rgbHex.slice(4,6), 16 );
-
-	L = Math.sqrt ( .241 * ( R * R ) + .691 * ( G * G ) + .068 * ( B * B) ) / 255;
-
-	// Convert to 0-1 values
-	R /= 255;
-	G /= 255;
-	B /= 255;
-
 	// Get basics for conversion
-	var max = Math.max(R, G, B), min = Math.min(R, G, B);
-	var H, S, V = max;
+	r /= 255;
+    g /= 255;
+    b /= 255;
+
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, v = max;
 	var d = max - min;
 
-	S = max == 0 ? 0 : d / max;
+	// Lightness value
+	l = 0.5 * (max + min);
+
+	// Luma value
+	y = (0.30 * r) + ( 0.59 * g ) + ( 0.11 * b );
+
+	s = max == 0 ? 0 : d / max;
 
 	if (d == 0) {
-		H = 0;
+		h = 0;
 	} else {
 		switch ( max ) {
-			case R: H = ( G - B ) / d + ( G < B ? 6 : 0 ); break;
-			case G: H = ( B - R ) / d + 2; break;
-			case B: H = ( R - G ) / d + 4; break;
+			case r: h = ( g - b ) / d + ( g < b ? 6 : 0 ); break;
+			case g: h = ( b - r ) / d + 2; break;
+			case b: h = ( r - g ) / d + 4; break;
 		}
 
-		H /= 6;
+		h /= 6;
 	}
 
-	return [H,S,V,L];
+	return [h,s,v,l,y];
+}
+
+function hsv2rgb ( h, s, v )
+{
+	h *= 360;
+	var c = s * v;
+	var h1 = h / 60;
+	var x = c * ( 1 - Math.abs( h1 % 2 - 1) );
+
+	if (h1 < 1) {
+		rgb = [ c, x, 0 ];
+	} else if ( h1 < 2 ) {
+		rgb = [ x, c, 0 ];
+	} else if ( h1 < 3 ) {
+		rgb = [ 0, c, x ];
+	} else if ( h1 < 4 ) {
+		rgb = [ 0, x, c ];
+	} else if ( h1 < 5 ) {
+		rgb = [ x, 0, c ];
+	} else if ( h1 < 6 ) {
+		rgb = [ c, 0, x ];
+	}
+
+	var m = v - c;
+	rgb = [rgb[0] + m, rgb[1] + m, rgb[2] + m];
+
+	rgb[0] = Math.round(rgb[0] * 255);
+	rgb[1] = Math.round(rgb[1] * 255);
+	rgb[2] = Math.round(rgb[2] * 255);
+
+	return rgb;
 }
 
 
@@ -118,16 +138,78 @@ function hsl2rgb ( h, s, l )
 	var m = l - (c * .5);
 	rgb = [rgb[0] + m, rgb[1] + m, rgb[2] + m];
 
-	var r = Math.round(rgb[0] * 255).toString(16);
-	var g = Math.round(rgb[1] * 255).toString(16);
-	var b = Math.round(rgb[2] * 255).toString(16);
+	rgb[0] = Math.round(rgb[0] * 255);
+	rgb[1] = Math.round(rgb[1] * 255);
+	rgb[2] = Math.round(rgb[2] * 255);
+
+	return rgb;
+}
+
+function hcy2rgb ( h, c, y )
+{
+	h *= 360;
+	var h1 = h / 60;
+	var x = c * ( 1 - Math.abs( h1 % 2 - 1) );
+
+	if (h1 < 1) {
+		rgb = [ c, x, 0 ];
+	} else if ( h1 < 2 ) {
+		rgb = [ x, c, 0 ];
+	} else if ( h1 < 3 ) {
+		rgb = [ 0, c, x ];
+	} else if ( h1 < 4 ) {
+		rgb = [ 0, x, c ];
+	} else if ( h1 < 5 ) {
+		rgb = [ x, 0, c ];
+	} else if ( h1 < 6 ) {
+		rgb = [ c, 0, x ];
+	}
+
+	var m = y - ( 0.30 * rgb[0] ) + ( 0.59 * rgb[1] ) + ( 0.11 * rgb[2] );
+	rgb = [rgb[0] + m, rgb[1] + m, rgb[2] + m];
+
+	rgb[0] = Math.round(rgb[0] * 255);
+	rgb[1] = Math.round(rgb[1] * 255);
+	rgb[2] = Math.round(rgb[2] * 255);
+
+	return rgb;
+}
+
+function getRGBString ( r, g, b )
+{
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
 
 	if (r.length == 1) r = '0' + r;
 	if (g.length == 1) g = '0' + g;
 	if (b.length == 1) b = '0' + b;
 
 	var rgbString = r + g + b;
-
 	return rgbString;
 }
 
+function getRGBArray ( rgbHex )
+{
+	// strip #
+	if (rgbHex.slice(0,1) == '#') rgbHex = rgbHex.slice(1);
+
+	// Parse to 0-255 values
+	r = parseInt( rgbHex.slice(0,2), 16 );
+	g = parseInt( rgbHex.slice(2,4), 16 );
+	b = parseInt( rgbHex.slice(4,6), 16 );
+	return [r, g, b];
+}
+
+function getYfromRGB ( r, g, b )
+{
+	// Get basics for conversion
+	r /= 255;
+    g /= 255;
+    b /= 255;
+
+	// Luma value
+	y = (0.30 * r) + ( 0.59 * g ) + ( 0.11 * b );
+
+	return y;
+}
--- scripts.js
+++ scripts.js
@@ -1,32 +1,60 @@
 function updateColors ( element )
 {
-	var hsvl, new_hsvl, rgb, compliment;
+	var hsvl, hsvl_comp, hsvl_comp_tint, hsvl_comp_shade, hsvl_base_tint,
+		rgb_base, rgb_comp, rgb_comp_tint, rgb_comp_shade, rgb_base_tint;
 
 	rgb = getAverageRGB( element );
-	console.log ( 'Image Average RGB: #' + rgb );
-
-	// Set background color to average RGB for easy viewing
-	document.body.style.backgroundColor = rgb;
 
 	// Get HSVL of RGB
-	hsvl = rgb2hsvl ( rgb );
+	base          = rgb2hsvly ( rgb[0], rgb[1], rgb[2] );
+	comp          = base.slice(0);
+	comp_tint     = base.slice(0);
+	comp_shade    = base.slice(0);
+	base_tint     = base.slice(0);
 
-	new_hsvl = hsvl.slice(0);
+	// Find the rest of the color palette
+	comp[0]       = (comp[0] + .5) % 1;
+	comp_tint[0]  = (comp_tint[0] + .5) % 1;
+	comp_tint[2]  = Math.min( (comp_tint[2] + .2), 1);
+	comp_shade[0] = (comp_shade[0] + .5) % 1;
+	comp_shade[2] = Math.max( (comp_shade[2] - .2), 0);
+	base_tint[2]  = Math.min( (base_tint[2] + .2), 1);
 
-	// Find complimentary color
-	new_hsvl[0] += .5;
-	if (new_hsvl[0] > 1) new_hsvl[0] -= 1;
+	// Get RGBs
+	rgb_base           = rgb;
+	rgb_comp           = hsv2rgb ( comp[0],       comp[1],       comp[3] );
+	rgb_comp_tint      = hsv2rgb ( comp_tint[0],  comp_tint[1],  comp_tint[2] );
+	rgb_comp_shade     = hsv2rgb ( comp_shade[0], comp_shade[1], comp_shade[2] );
+	rgb_base_tint      = hsv2rgb ( base_tint[0],  base_tint[1],  base_tint[2] );
 
-	// Find reverse lumi
-	new_hsvl[3] = (new_hsvl[3] + .5) % 1;
-
-	// Get RGB of compliment
-	compliment = hsl2rgb ( new_hsvl[0], new_hsvl[1], new_hsvl[3] );
-
-	console.log ( 'Image Compliment RGB: #' + compliment );
+	// Get hex values for all the RGBs
+	hex_base = getRGBString( rgb_base[0], rgb_base[1], rgb_base[2] );
+    hex_base_tint = getRGBString( rgb_base_tint[0], rgb_base_tint[1], rgb_base_tint[2] );
+	hex_comp = getRGBString( rgb_comp[0], rgb_comp[1], rgb_comp[2] );
+    hex_comp_tint = getRGBString( rgb_comp_tint[0], rgb_comp_tint[1], rgb_comp_tint[2] );
+    hex_comp_shade = getRGBString( rgb_comp_shade[0], rgb_comp_shade[1], rgb_comp_shade[2] );
 
 	// Style text with it
-	document.getElementById('header').style.color = compliment;
+	$('#color1').css( 'background-color', hex_base );
+	$('#color2').css( 'background-color', hex_base_tint);
+	$('#color3').css( 'background-color', hex_comp_tint);
+	$('#color4').css( 'background-color', hex_comp);
+	$('#color5').css( 'background-color', hex_comp_shade);
+
+	// Add hex value in text to boxes
+	$('#color1 .label').html( '#' + hex_base );
+	$('#color2 .label').html( '#' + hex_base_tint );
+	$('#color3 .label').html( '#' + hex_comp_tint );
+	$('#color4 .label').html( '#' + hex_comp );
+	$('#color5 .label').html( '#' + hex_comp_shade );
+
+	// Change text color by lumen value of bg color
+	$('#color1').css ( 'color', ( getYfromRGB( rgb_base[0], rgb_base[1], rgb_base[2] ) > .5 ) ? '#000' : '#FFF' );
+	$('#color2').css ( 'color', ( getYfromRGB( rgb_base_tint[0], rgb_base_tint[1], rgb_base_tint[2] ) > .5 ) ? '#000' : '#FFF' );
+	$('#color3').css ( 'color', ( getYfromRGB( rgb_comp_tint[0], rgb_comp_tint[1], rgb_comp_tint[2] ) > .5 ) ? '#000' : '#FFF' );
+	$('#color4').css ( 'color', ( getYfromRGB( rgb_comp[0], rgb_comp[1], rgb_comp[2] ) > .5 ) ? '#000' : '#FFF' );
+	$('#color5').css ( 'color', ( getYfromRGB( rgb_comp_shade[0], rgb_comp_shade[1], rgb_comp_shade[2] ) > .5 ) ? '#000' : '#FFF' );
+
 }
 
 function thumbClick ( image )
